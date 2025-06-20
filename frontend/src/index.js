a30cee00b8beaaf36617bc3972f45c21d566998f
Abhishek Chauhan <124580596+abhishekcha790@users.noreply.github.com>
2025-06-20 19:26:29 +0530

    Login folder differentitation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ReactDOM from "react-dom/client";
 import Home from "./components/Home";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 
 const router = createBrowserRouter([
   {
