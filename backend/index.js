85479f322098de50223719093064e0b30a7cee59
Abhishek Chauhan <124580596+abhishekcha790@users.noreply.github.com>
2025-06-20 12:08:10 +0530

    db connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+DB_URL =
+  "mongodb+srv://abhisheksinghc84:OBVfQch6jNCBvTKZ@cluster0.jkkpgsi.mongodb.net/";
+
+mongoose.connect(DB_URL);
+const conn = mongoose.connection;
+
+conn.once("open", () => {
+  console.log("successfully database connected");
+});
+
+conn.on("error", () => {
+  console.log("error connecting to database");
+  process.exit();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
