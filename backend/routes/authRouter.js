7ebbe9d132e220e81c14ec72674b08cc69ea0475
Aditaty2002 <yadavaditay27@gmail.com>
2025-07-12 06:08:46 -0400

    login fully functional

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { googleLogin } = require("../controllers/authControllers");
 const router = express.Router();
 require("dotenv").config();
 const { sendOtp, verifyOtp } = require("../controllers/authControllerPhone");
@@ -7,6 +8,7 @@ const { sendOtp, verifyOtp } = require("../controllers/authControllerPhone");
 
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
+router.post("/google", googleLogin);
 
 
 module.exports = router;
